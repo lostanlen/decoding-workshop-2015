6c5885ddcf7eb739c97c2057d6758fa94a2d8165
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-10-29 22:14:11 +0100

    start writing switch-case statement

--- decoding_workshop_script.m
+++ decoding_workshop_script.m
@@ -13,4 +13,8 @@ file_names = {audio_files([audio_files.bytes]>0).name};
 nFiles = length(file_names);
 file_name = file_names{file_index};
 file_path = ['media/', file_name];
-synthesize_mcdermott(file_name);
+
+switch method_index
+    case 2
+        synthesize_mcdermott(file_name);
+end
